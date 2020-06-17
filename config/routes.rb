332d2a8192c376a311c849bfc8b6b1070be736a2
阿部爽 <akira1225marathon@gmail.com>
2020-06-17 10:50:26 +0000

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,16 @@ Rails.application.routes.draw do
   get 'pages/help'
   
   resources :users
-  
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-  
-  resources :users
   resources :topics
+  
+  get 'comments' => 'comments#create' 
   
   get 'favorites/index'
   post '/favorites', to: 'favorites#create'
   delete '/favorites', to: 'favorites#destroy'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
 end
